b45f6510c262dda990405a0246c01ae14397d8ca
Matt Guerrette <direct3Dtutorials@gmail.com>
2021-05-25 17:46:48 -0400

    Fix extern declarations

--- src/CD3D12MemoryAllocator.h
+++ src/CD3D12MemoryAllocator.h
@@ -2,108 +2,117 @@
 #ifndef CD3D12_MEMORY_ALLOCATOR_H_
 #define CD3D12_MEMORY_ALLOCATOR_H_
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 #include <d3d12.h>
 #include <dxgi.h>
 
-typedef void *(*CD3D12MA_ALLOCATE_FUNC_PTR) (size_t Size, size_t Alignment, void *pUserData);
-typedef void (*CD3D12MA_FREE_FUNC_PTR) (void *pMemory, void *pUserData);
+  typedef void *(*CD3D12MA_ALLOCATE_FUNC_PTR) (size_t Size, size_t Alignment, void *pUserData);
+  typedef void (*CD3D12MA_FREE_FUNC_PTR) (void *pMemory, void *pUserData);
 
-typedef struct CD3D12MAAllocator CD3D12MAAllocator;
+  typedef struct CD3D12MAAllocator CD3D12MAAllocator;
 
-typedef struct
-{
-  CD3D12MA_ALLOCATE_FUNC_PTR pAllocate;
-  CD3D12MA_FREE_FUNC_PTR pFree;
-  void *pUserData;
-} CD3D12MA_ALLOCATION_CALLBACKS;
+  typedef struct
+  {
+    CD3D12MA_ALLOCATE_FUNC_PTR pAllocate;
+    CD3D12MA_FREE_FUNC_PTR pFree;
+    void *pUserData;
+  } CD3D12MA_ALLOCATION_CALLBACKS;
 
-typedef struct
-{
-  /// Number of memory blocks (heaps) allocated.
-  UINT BlockCount;
-  /// Number of D3D12MA::Allocation objects allocated.
-  UINT AllocationCount;
-  /// Number of free ranges of memory between allocations.
-  UINT UnusedRangeCount;
-  /// Total number of bytes occupied by all allocations.
-  UINT64 UsedBytes;
-  /// Total number of bytes occupied by unused ranges.
-  UINT64 UnusedBytes;
-  UINT64 AllocationSizeMin;
-  UINT64 AllocationSizeAvg;
-  UINT64 AllocationSizeMax;
-  UINT64 UnusedRangeSizeMin;
-  UINT64 UnusedRangeSizeAvg;
-  UINT64 UnusedRangeSizeMax;
-} CD3D12MA_STAT_INFO;
+  typedef struct
+  {
+    /// Number of memory blocks (heaps) allocated.
+    UINT BlockCount;
+    /// Number of D3D12MA::Allocation objects allocated.
+    UINT AllocationCount;
+    /// Number of free ranges of memory between allocations.
+    UINT UnusedRangeCount;
+    /// Total number of bytes occupied by all allocations.
+    UINT64 UsedBytes;
+    /// Total number of bytes occupied by unused ranges.
+    UINT64 UnusedBytes;
+    UINT64 AllocationSizeMin;
+    UINT64 AllocationSizeAvg;
+    UINT64 AllocationSizeMax;
+    UINT64 UnusedRangeSizeMin;
+    UINT64 UnusedRangeSizeAvg;
+    UINT64 UnusedRangeSizeMax;
+  } CD3D12MA_STAT_INFO;
 
 #define CD3D12MA_HEAP_TYPE_COUNT 4
-typedef struct
-{
-  CD3D12MA_STAT_INFO Total;
-  CD3D12MA_STAT_INFO HeapType[CD3D12MA_HEAP_TYPE_COUNT];
-} CD3D12MA_STATS;
+  typedef struct
+  {
+    CD3D12MA_STAT_INFO Total;
+    CD3D12MA_STAT_INFO HeapType[CD3D12MA_HEAP_TYPE_COUNT];
+  } CD3D12MA_STATS;
 
-typedef struct
-{
-  UINT64 BlockBytes;
-  UINT64 AllocationBytes;
-  UINT64 UsageBytes;
-  UINT64 BudgetBytes;
-} CD3D12MA_BUDGET;
+  typedef struct
+  {
+    UINT64 BlockBytes;
+    UINT64 AllocationBytes;
+    UINT64 UsageBytes;
+    UINT64 BudgetBytes;
+  } CD3D12MA_BUDGET;
 
-typedef enum
-{
-  CD3D12MA_ALLOCATOR_FLAG_NONE = 0,
-  CD3D12MA_ALLOCATOR_FLAG_SINGLETHREADED = 0x1,
-  CD3D12MA_ALLOCATOR_FLAG_ALWAYS_COMMITTED = 0x2
-} CD3D12MA_ALLOCATOR_FLAGS;
+  typedef enum
+  {
+    CD3D12MA_ALLOCATOR_FLAG_NONE = 0,
+    CD3D12MA_ALLOCATOR_FLAG_SINGLETHREADED = 0x1,
+    CD3D12MA_ALLOCATOR_FLAG_ALWAYS_COMMITTED = 0x2
+  } CD3D12MA_ALLOCATOR_FLAGS;
 
-typedef struct
-{
-  CD3D12MA_ALLOCATOR_FLAGS Flags;
-  ID3D12Device *pDevice;
-  UINT64 PreferredBlockSize;
-  const CD3D12MA_ALLOCATION_CALLBACKS *pAllocationCallbacks;
-  IDXGIAdapter *pAdapter;
-} CD3D12MA_ALLOCATOR_DESC;
+  typedef struct
+  {
+    CD3D12MA_ALLOCATOR_FLAGS Flags;
+    ID3D12Device *pDevice;
+    UINT64 PreferredBlockSize;
+    const CD3D12MA_ALLOCATION_CALLBACKS *pAllocationCallbacks;
+    IDXGIAdapter *pAdapter;
+  } CD3D12MA_ALLOCATOR_DESC;
 
-typedef struct
-{
-  D3D12_HEAP_PROPERTIES HeapProperties;
-  D3D12_HEAP_FLAGS HeapFlags;
-  UINT64 BlockSize;
-  UINT MinBlockCount;
-  UINT MaxBlockCount;
-} CD3D12MA_POOL_DESC;
+  typedef struct
+  {
+    D3D12_HEAP_PROPERTIES HeapProperties;
+    D3D12_HEAP_FLAGS HeapFlags;
+    UINT64 BlockSize;
+    UINT MinBlockCount;
+    UINT MaxBlockCount;
+  } CD3D12MA_POOL_DESC;
 
-typedef struct CD3D12MAPoolVtbl
-{
-  void (*Release) ();
-} CD3D12MAPoolVtbl;
+  typedef struct CD3D12MAPoolVtbl
+  {
+    void (*Release) ();
+  } CD3D12MAPoolVtbl;
 
-typedef struct CD3D12MAPool
-{
-  /*<private>*/
-  const CD3D12MAPoolVtbl *pVtbl;
-  void *pInstance;
-} CD3D12MAPool;
+  typedef struct CD3D12MAPool
+  {
+    /*<private>*/
+    const CD3D12MAPoolVtbl *pVtbl;
+    void *pInstance;
+  } CD3D12MAPool;
 
-typedef struct CD3D12MAAllocatorVtbl
-{
-  void (*Release) (CD3D12MAAllocator *This);
-} CD3D12MAAllocatorVtbl;
+  typedef struct CD3D12MAAllocatorVtbl
+  {
+    void (*Release) (CD3D12MAAllocator *This);
+  } CD3D12MAAllocatorVtbl;
 
-struct CD3D12MAAllocator
-{
-  /*<private>*/
-  const CD3D12MAAllocatorVtbl *pVtbl;
-  void *pInstance;
-};
+  struct CD3D12MAAllocator
+  {
+    /*<private>*/
+    const CD3D12MAAllocatorVtbl *pVtbl;
+    void *pInstance;
+  };
 
 #define CD3D12MAAllocator_Release(this) (this->pVtbl)->Release (this)
 
-HRESULT
-CreateAllocator (const CD3D12MA_ALLOCATOR_DESC *pDesc, CD3D12MAAllocator **ppAllocator);
+  HRESULT
+  CreateAllocator (const CD3D12MA_ALLOCATOR_DESC *pDesc, CD3D12MAAllocator **ppAllocator);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
