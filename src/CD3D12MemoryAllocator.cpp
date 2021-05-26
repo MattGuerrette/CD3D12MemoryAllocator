a722ad9e76c1448dbaec5f9409beb951e24a8c09
Matt Guerrette <direct3Dtutorials@gmail.com>
2021-05-25 22:02:37 -0400

    Initial Pool, Allocation, and Allocator C interface

--- src/CD3D12MemoryAllocator.cpp
+++ src/CD3D12MemoryAllocator.cpp
@@ -16,6 +16,106 @@ static_assert (
     "Size mismatch: sizeof(CD3D12MA_ALLOCATOR_DESC) != sizeof(D3D12MA::ALLOCATOR_DESC)");
 static_assert (sizeof (CD3D12MA_POOL_DESC) == sizeof (D3D12MA::POOL_DESC),
                "Size mismatch: sizeof(CD3D12MA_POOL_DESC) != sizeof(D3D12MA::POOL_DESC)");
+static_assert (
+    sizeof (CD3D12MA_ALLOCATION_DESC) == sizeof (D3D12MA::ALLOCATION_DESC),
+    "Size mismatch: sizeof(CD3D12MA_ALLOCATION_DESC) != sizeof(D3D12MA::ALLOCATION_DESC)");
+
+static void
+CD3D12MAAllocationRelease (CD3D12MAAllocation *pAllocation)
+{
+  reinterpret_cast<D3D12MA::Allocation *> (pAllocation->pInstance)->Release ();
+  free (pAllocation);
+}
+
+static UINT64
+CD3D12MAAllocationGetOffset (CD3D12MAAllocation *pAllocation)
+{
+  return reinterpret_cast<D3D12MA::Allocation *> (pAllocation->pInstance)->GetOffset ();
+}
+
+static UINT64
+CD3D12MAAllocationGetSize (CD3D12MAAllocation *pAllocation)
+{
+  return reinterpret_cast<D3D12MA::Allocation *> (pAllocation->pInstance)->GetSize ();
+}
+
+static ID3D12Resource *
+CD3D12MAAllocationGetResource (CD3D12MAAllocation *pAllocation)
+{
+  return reinterpret_cast<D3D12MA::Allocation *> (pAllocation->pInstance)->GetResource ();
+}
+
+static ID3D12Heap *
+CD3D12MAAllocationGetHeap (CD3D12MAAllocation *pAllocation)
+{
+  return reinterpret_cast<D3D12MA::Allocation *> (pAllocation->pInstance)->GetHeap ();
+}
+
+static void
+CD3D12MAAllocationSetName (CD3D12MAAllocation *pAllocation, LPCWSTR Name)
+{
+  reinterpret_cast<D3D12MA::Allocation *> (pAllocation->pInstance)->SetName (Name);
+}
+
+static LPCWSTR
+CD3D12MAAllocationGetName (CD3D12MAAllocation *pAllocation)
+{
+  return reinterpret_cast<D3D12MA::Allocation *> (pAllocation->pInstance)->GetName ();
+}
+
+static bool
+CD3D12MAAllocationWasZeroInitialized (CD3D12MAAllocation *pAllocation)
+{
+  return reinterpret_cast<D3D12MA::Allocation *> (pAllocation->pInstance)->WasZeroInitialized ();
+}
+
+static const CD3D12MAAllocationVtbl g_CD3D12MAAllocationVtbl = {
+  CD3D12MAAllocationRelease,     CD3D12MAAllocationGetOffset,         CD3D12MAAllocationGetSize,
+  CD3D12MAAllocationGetResource, CD3D12MAAllocationGetHeap,           CD3D12MAAllocationSetName,
+  CD3D12MAAllocationGetName,     CD3D12MAAllocationWasZeroInitialized
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+static void
+CD3D12MAPoolRelease (CD3D12MAPool *pPool)
+{
+  reinterpret_cast<D3D12MA::Pool *> (pPool->pInstance)->Release ();
+  free (pPool);
+}
+
+static void
+CD3D12MAPoolGetDesc (CD3D12MAPool *pPool, CD3D12MA_POOL_DESC *pOut)
+{
+  D3D12MA::POOL_DESC pool_desc = reinterpret_cast<D3D12MA::Pool *> (pPool->pInstance)->GetDesc ();
+  *pOut = *reinterpret_cast<CD3D12MA_POOL_DESC *> (&pool_desc);
+}
+
+static void
+CD3D12MAPoolCalculateStats (CD3D12MAPool *pPool, CD3D12MA_STAT_INFO *pOut)
+{
+  D3D12MA::StatInfo stats_info = {};
+  reinterpret_cast<D3D12MA::Pool *> (pPool->pInstance)->CalculateStats (&stats_info);
+  *pOut = *reinterpret_cast<CD3D12MA_STAT_INFO *> (&stats_info);
+}
+
+static void
+CD3D12MAPoolSetName (CD3D12MAPool *pPool, LPCWSTR Name)
+{
+  reinterpret_cast<D3D12MA::Pool *> (pPool->pInstance)->SetName (Name);
+}
+
+static LPCWSTR
+CD3D12MAPoolGetName (CD3D12MAPool *pPool)
+{
+  return reinterpret_cast<D3D12MA::Pool *> (pPool->pInstance)->GetName ();
+}
+
+static const CD3D12MAPoolVtbl g_CD3D12MAPoolVtbl = { CD3D12MAPoolRelease, CD3D12MAPoolGetDesc,
+                                                     CD3D12MAPoolCalculateStats,
+                                                     CD3D12MAPoolSetName, CD3D12MAPoolGetName };
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
 static void
 CD3D12MAAllocatorRelease (CD3D12MAAllocator *pAllocator)
@@ -31,8 +131,291 @@ CD3D12MAAllocatorGetD3D12Options (CD3D12MAAllocator *pAllocator,
   *ppOptions = &reinterpret_cast<D3D12MA::Allocator *> (pAllocator->pInstance)->GetD3D12Options ();
 }
 
-static const CD3D12MAAllocatorVtbl g_CD3D12MAAllocatorVtbl = { CD3D12MAAllocatorRelease,
-                                                               CD3D12MAAllocatorGetD3D12Options };
+static bool
+CD3D12MAAllocatorIsUMA (CD3D12MAAllocator *pAllocator)
+{
+  return reinterpret_cast<D3D12MA::Allocator *> (pAllocator->pInstance)->IsUMA ();
+}
+
+static bool
+CD3D12MAAllocatorIsCacheCoherentUMA (CD3D12MAAllocator *pAllocator)
+{
+  return reinterpret_cast<D3D12MA::Allocator *> (pAllocator->pInstance)->IsCacheCoherentUMA ();
+}
+
+static HRESULT
+CD3D12MAAllocatorCreateResource (CD3D12MAAllocator *pAllocator,
+                                 const CD3D12MA_ALLOCATION_DESC *pAllocDesc,
+                                 const D3D12_RESOURCE_DESC *pResourceDesc,
+                                 D3D12_RESOURCE_STATES InitialResourceState,
+                                 const D3D12_CLEAR_VALUE *pOptimizedClearValue,
+                                 CD3D12MAAllocation **ppAllocation,
+                                 REFIID iidResource,
+                                 void **ppvResource)
+{
+  if (!pAllocator)
+    {
+      return E_INVALIDARG;
+    }
+
+  D3D12MA::ALLOCATION_DESC allocation_desc = *const_cast<D3D12MA::ALLOCATION_DESC *> (
+      reinterpret_cast<const D3D12MA::ALLOCATION_DESC *> (pAllocDesc));
+  if (pAllocDesc->CustomPool != nullptr)
+    {
+      allocation_desc.CustomPool =
+          reinterpret_cast<D3D12MA::Pool *> (pAllocDesc->CustomPool->pInstance);
+    }
+
+  D3D12MA::Allocation *allocation = nullptr;
+  HRESULT hResult =
+      reinterpret_cast<D3D12MA::Allocator *> (pAllocator->pInstance)
+          ->CreateResource (&allocation_desc, pResourceDesc, InitialResourceState,
+                            pOptimizedClearValue, &allocation, iidResource, ppvResource);
+  *ppAllocation = static_cast<CD3D12MAAllocation *> (malloc (sizeof (CD3D12MAAllocation)));
+  (*ppAllocation)->pVtbl = &g_CD3D12MAAllocationVtbl;
+  (*ppAllocation)->pInstance = allocation;
+
+  return hResult;
+}
+
+#ifdef __ID3D12Device4_INTERFACE_DEFINED__
+static HRESULT
+CD3D12MAAllocatorCreateResource1 (CD3D12MAAllocator *pAllocator,
+                                  const CD3D12MA_ALLOCATION_DESC *pAllocDesc,
+                                  const D3D12_RESOURCE_DESC *pResourceDesc,
+                                  D3D12_RESOURCE_STATES InitialResourceState,
+                                  const D3D12_CLEAR_VALUE *pOptimizedClearValue,
+                                  ID3D12ProtectedResourceSession *pProtectedSession,
+                                  CD3D12MAAllocation **ppAllocation,
+                                  REFIID iidResource,
+                                  void **ppvResource)
+{
+  if (!pAllocator)
+    {
+      return E_INVALIDARG;
+    }
+
+  D3D12MA::ALLOCATION_DESC allocation_desc = *const_cast<D3D12MA::ALLOCATION_DESC *> (
+      reinterpret_cast<const D3D12MA::ALLOCATION_DESC *> (pAllocDesc));
+  if (pAllocDesc->CustomPool != nullptr)
+    {
+      allocation_desc.CustomPool =
+          reinterpret_cast<D3D12MA::Pool *> (pAllocDesc->CustomPool->pInstance);
+    }
+
+  D3D12MA::Allocation *allocation = nullptr;
+  HRESULT hResult = reinterpret_cast<D3D12MA::Allocator *> (pAllocator->pInstance)
+                        ->CreateResource1 (&allocation_desc, pResourceDesc, InitialResourceState,
+                                           pOptimizedClearValue, pProtectedSession, &allocation,
+                                           iidResource, ppvResource);
+  *ppAllocation = static_cast<CD3D12MAAllocation *> (malloc (sizeof (CD3D12MAAllocation)));
+  (*ppAllocation)->pVtbl = &g_CD3D12MAAllocationVtbl;
+  (*ppAllocation)->pInstance = allocation;
+
+  return hResult;
+}
+#endif
+
+#ifdef __ID3D12Device8_INTERFACE_DEFINED__
+static HRESULT
+CD3D12MAAllocatorCreateResource2 (CD3D12MAAllocator *pAllocator,
+                                  const CD3D12MA_ALLOCATION_DESC *pAllocDesc,
+                                  const D3D12_RESOURCE_DESC1 *pResourceDesc,
+                                  D3D12_RESOURCE_STATES InitialResourceState,
+                                  const D3D12_CLEAR_VALUE *pOptimizedClearValue,
+                                  ID3D12ProtectedResourceSession *pProtectedSession,
+                                  CD3D12MAAllocation **ppAllocation,
+                                  REFIID iidResource,
+                                  void **ppvResource)
+{
+  if (!pAllocator)
+    {
+      return E_INVALIDARG;
+    }
+
+  D3D12MA::ALLOCATION_DESC allocation_desc = *const_cast<D3D12MA::ALLOCATION_DESC *> (
+      reinterpret_cast<const D3D12MA::ALLOCATION_DESC *> (pAllocDesc));
+  if (pAllocDesc->CustomPool != nullptr)
+    {
+      allocation_desc.CustomPool =
+          reinterpret_cast<D3D12MA::Pool *> (pAllocDesc->CustomPool->pInstance);
+    }
+
+  D3D12MA::Allocation *allocation = nullptr;
+  HRESULT hResult = reinterpret_cast<D3D12MA::Allocator *> (pAllocator->pInstance)
+                        ->CreateResource2 (&allocation_desc, pResourceDesc, InitialResourceState,
+                                           pOptimizedClearValue, pProtectedSession, &allocation,
+                                           iidResource, ppvResource);
+  *ppAllocation = static_cast<CD3D12MAAllocation *> (malloc (sizeof (CD3D12MAAllocation)));
+  (*ppAllocation)->pVtbl = &g_CD3D12MAAllocationVtbl;
+  (*ppAllocation)->pInstance = allocation;
+
+  return hResult;
+}
+#endif
+
+static HRESULT
+CD3D12MAAllocatorAllocateMemory (CD3D12MAAllocator *pAllocator,
+                                 const CD3D12MA_ALLOCATION_DESC *pAllocDesc,
+                                 const D3D12_RESOURCE_ALLOCATION_INFO *pAllocInfo,
+                                 CD3D12MAAllocation **ppAllocation)
+{
+  if (!pAllocator)
+    {
+      return E_INVALIDARG;
+    }
+
+  D3D12MA::ALLOCATION_DESC allocation_desc = *const_cast<D3D12MA::ALLOCATION_DESC *> (
+      reinterpret_cast<const D3D12MA::ALLOCATION_DESC *> (pAllocDesc));
+  if (pAllocDesc->CustomPool != nullptr)
+    {
+      allocation_desc.CustomPool =
+          reinterpret_cast<D3D12MA::Pool *> (pAllocDesc->CustomPool->pInstance);
+    }
+
+  D3D12MA::Allocation *allocation = nullptr;
+  HRESULT hResult = reinterpret_cast<D3D12MA::Allocator *> (pAllocator->pInstance)
+                        ->AllocateMemory (&allocation_desc, pAllocInfo, &allocation);
+  *ppAllocation = static_cast<CD3D12MAAllocation *> (malloc (sizeof (CD3D12MAAllocation)));
+  (*ppAllocation)->pVtbl = &g_CD3D12MAAllocationVtbl;
+  (*ppAllocation)->pInstance = allocation;
+
+  return hResult;
+}
+
+#ifdef __ID3D12Device4_INTERFACE_DEFINED__
+static HRESULT
+CD3D12MAAllocatorAllocateMemory1 (CD3D12MAAllocator *pAllocator,
+                                  const CD3D12MA_ALLOCATION_DESC *pAllocDesc,
+                                  const D3D12_RESOURCE_ALLOCATION_INFO *pAllocInfo,
+                                  ID3D12ProtectedResourceSession *pProtectedSession,
+                                  CD3D12MAAllocation **ppAllocation)
+{
+  if (!pAllocator)
+    {
+      return E_INVALIDARG;
+    }
+
+  D3D12MA::ALLOCATION_DESC allocation_desc = *const_cast<D3D12MA::ALLOCATION_DESC *> (
+      reinterpret_cast<const D3D12MA::ALLOCATION_DESC *> (pAllocDesc));
+  if (pAllocDesc->CustomPool != nullptr)
+    {
+      allocation_desc.CustomPool =
+          reinterpret_cast<D3D12MA::Pool *> (pAllocDesc->CustomPool->pInstance);
+    }
+
+  D3D12MA::Allocation *allocation = nullptr;
+  HRESULT hResult =
+      reinterpret_cast<D3D12MA::Allocator *> (pAllocator->pInstance)
+          ->AllocateMemory1 (&allocation_desc, pAllocInfo, pProtectedSession, &allocation);
+  *ppAllocation = static_cast<CD3D12MAAllocation *> (malloc (sizeof (CD3D12MAAllocation)));
+  (*ppAllocation)->pVtbl = &g_CD3D12MAAllocationVtbl;
+  (*ppAllocation)->pInstance = allocation;
+
+  return hResult;
+}
+#endif
+
+static HRESULT
+CD3D12MAAllocatorCreateAliasingResource (CD3D12MAAllocator *pAllocator,
+                                         CD3D12MAAllocation *pAllocation,
+                                         UINT64 AllocationLocalOffset,
+                                         const D3D12_RESOURCE_DESC *pResourceDesc,
+                                         D3D12_RESOURCE_STATES InitialResourceState,
+                                         const D3D12_CLEAR_VALUE *pOptimizedClearValue,
+                                         REFIID iidResource,
+                                         void **ppvResource)
+{
+  if (!pAllocator)
+    {
+      return E_INVALIDARG;
+    }
+
+  HRESULT hResult = reinterpret_cast<D3D12MA::Allocator *> (pAllocator->pInstance)
+                        ->CreateAliasingResource (
+                            reinterpret_cast<D3D12MA::Allocation *> (pAllocation->pInstance),
+                            AllocationLocalOffset, pResourceDesc, InitialResourceState,
+                            pOptimizedClearValue, iidResource, ppvResource);
+
+  return hResult;
+}
+
+static HRESULT
+CD3D12MAAllocatorCreatePool (CD3D12MAAllocator *pAllocator,
+                             const CD3D12MA_POOL_DESC *pPoolDesc,
+                             CD3D12MAPool **ppPool)
+{
+  if (!pAllocator)
+    {
+      return E_INVALIDARG;
+    }
+
+  D3D12MA::Pool *pool = nullptr;
+  HRESULT hResult =
+      reinterpret_cast<D3D12MA::Allocator *> (pAllocator->pInstance)
+          ->CreatePool (reinterpret_cast<const D3D12MA::POOL_DESC *> (pPoolDesc), &pool);
+  *ppPool = static_cast<CD3D12MAPool *> (malloc (sizeof (CD3D12MAPool)));
+  (*ppPool)->pVtbl = &g_CD3D12MAPoolVtbl;
+  (*ppPool)->pInstance = pool;
+
+  return hResult;
+}
+
+static void
+CD3D12MAAllocatorSetCurrentFrameIndex (CD3D12MAAllocator *pAllocator, UINT frameIndex)
+{
+  reinterpret_cast<D3D12MA::Allocator *> (pAllocator->pInstance)
+      ->SetCurrentFrameIndex (frameIndex);
+}
+
+static void
+CD3D12MAAllocatorCalculateStats (CD3D12MAAllocator *pAllocator, CD3D12MA_STATS *pStats)
+{
+  D3D12MA::Stats stats = {};
+  reinterpret_cast<D3D12MA::Allocator *> (pAllocator->pInstance)->CalculateStats (&stats);
+  *pStats = *reinterpret_cast<CD3D12MA_STATS *> (&stats);
+}
+
+static void
+CD3D12MAAllocatorGetBudget (CD3D12MAAllocator *pAllocator,
+                            CD3D12MA_BUDGET *pGpuBudget,
+                            CD3D12MA_BUDGET *pCpuBudget)
+{
+  D3D12MA::Budget gpu = {};
+  D3D12MA::Budget cpu = {};
+  reinterpret_cast<D3D12MA::Allocator *> (pAllocator->pInstance)->GetBudget (&gpu, &cpu);
+  *pGpuBudget = *reinterpret_cast<CD3D12MA_BUDGET *> (&gpu);
+  *pCpuBudget = *reinterpret_cast<CD3D12MA_BUDGET *> (&cpu);
+}
+
+static const CD3D12MAAllocatorVtbl g_CD3D12MAAllocatorVtbl = {
+  CD3D12MAAllocatorRelease,
+  CD3D12MAAllocatorGetD3D12Options,
+  CD3D12MAAllocatorIsUMA,
+  CD3D12MAAllocatorIsCacheCoherentUMA,
+  CD3D12MAAllocatorCreateResource
+#ifdef __ID3D12Device4_INTERFACE_DEFINED__
+  ,
+  CD3D12MAAllocatorCreateResource1
+#endif
+
+#ifdef __ID3D12Device8_INTERFACE_DEFINED__
+  ,
+  CD3D12MAAllocatorCreateResource2
+#endif
+  ,
+  CD3D12MAAllocatorAllocateMemory
+#ifdef __ID3D12Device4_INTERFACE_DEFINED__
+  ,
+  CD3D12MAAllocatorAllocateMemory1
+#endif
+  ,
+  CD3D12MAAllocatorCreateAliasingResource,
+  CD3D12MAAllocatorCreatePool,
+  CD3D12MAAllocatorSetCurrentFrameIndex,
+  CD3D12MAAllocatorCalculateStats,
+  CD3D12MAAllocatorGetBudget
+};
 
 HRESULT
 CreateAllocator (const CD3D12MA_ALLOCATOR_DESC *pDesc, CD3D12MAAllocator **ppAllocator)
--- src/CD3D12MemoryAllocator.h
+++ src/CD3D12MemoryAllocator.h
@@ -10,6 +10,7 @@
 #else
 #define BEGIN_DECLS
 #define END_DECLS
+#include <stdbool.h>
 #endif
 
 BEGIN_DECLS
@@ -21,6 +22,8 @@ typedef void *(*CD3D12MA_ALLOCATE_FUNC_PTR) (size_t Size, size_t Alignment, void
 typedef void (*CD3D12MA_FREE_FUNC_PTR) (void *pMemory, void *pUserData);
 
 typedef struct CD3D12MAAllocator CD3D12MAAllocator;
+typedef struct CD3D12MAAllocation CD3D12MAAllocation;
+typedef struct CD3D12MAPool CD3D12MAPool;
 
 typedef struct
 {
@@ -91,7 +94,11 @@ typedef struct
 
 typedef struct CD3D12MAPoolVtbl
 {
-  void (*Release) ();
+  void (*Release) (CD3D12MAPool *This);
+  void (*GetDesc) (CD3D12MAPool *This, CD3D12MA_POOL_DESC *pOut);
+  void (*CalculateStats) (CD3D12MAPool *This, CD3D12MA_STAT_INFO *pOut);
+  void (*SetName) (CD3D12MAPool *This, LPCWSTR Name);
+  LPCWSTR (*GetName) (CD3D12MAPool *This);
 } CD3D12MAPoolVtbl;
 
 typedef struct CD3D12MAPool
@@ -101,11 +108,137 @@ typedef struct CD3D12MAPool
   void *pInstance;
 } CD3D12MAPool;
 
+#define CD3D12MAPool_Release(this) (this->pVtbl)->Release (this)
+#define CD3D12MAPool_GetDesc(this, pOut) (this->pVtbl)->GetDesc (this, pOut)
+#define CD3D12MAPool_CalculateStats(this, pOut) (this->pVtbl)->CalculateStats (this, pOut)
+#define CD3D12MAPool_SetName(this, name) (this->pVtbl)->SetName (this, name)
+#define CD3D12MAPool_GetName(this) (this->pVtbl)->GetName (this)
+
+typedef enum
+{
+  CD3D12MA_ALLOCATION_FLAG_NONE = 0,
+  CD3D12MA_ALLOCATION_FLAG_COMMITTED = 0x1,
+  CD3D12MA_ALLOCATION_FLAG_NEVER_ALLOCATE = 0x2,
+  CD3D12MA_ALLOCATION_FLAG_WITHIN_BUDGET = 0x4
+} CD3D12MA_ALLOCATION_FLAGS;
+
+typedef struct
+{
+  CD3D12MA_ALLOCATION_FLAGS Flags;
+  D3D12_HEAP_TYPE HeapType;
+  D3D12_HEAP_FLAGS ExtraHeapFlags;
+  CD3D12MAPool *CustomPool;
+} CD3D12MA_ALLOCATION_DESC;
+
+typedef struct CD3D12MAAllocationVtbl
+{
+  void (*Release) (CD3D12MAAllocation *This);
+  UINT64 (*GetOffset) (CD3D12MAAllocation *This);
+  UINT64 (*GetSize) (CD3D12MAAllocation *This);
+  ID3D12Resource *(*GetResource) (CD3D12MAAllocation *This);
+  ID3D12Heap *(*GetHeap) (CD3D12MAAllocation *This);
+  void (*SetName) (CD3D12MAAllocation *This, LPCWSTR Name);
+  LPCWSTR (*GetName) (CD3D12MAAllocation *This);
+  bool (*WasZeroInitialized) (CD3D12MAAllocation *This);
+} CD3D12MAAllocationVtbl;
+
+struct CD3D12MAAllocation
+{
+  /*<private>*/
+  const CD3D12MAAllocationVtbl *pVtbl;
+  void *pInstance;
+};
+
+#define CD3D12MAAllocation_Release(this) (this->pVtbl)->Release (this)
+#define CD3D12MAAllocation_GetOffset(this) (this->pVtbl)->GetOffset (this)
+#define CD3D12MAAllocation_GetSize(this) (this->pVtbl)->GetSize (this)
+#define CD3D12MAAllocation_GetResource(this) (this->pVtbl)->GetResource (this)
+#define CD3D12MAAllocation_GetHeap(this) (this->pVtbl)->GetHeap (this)
+#define CD3D12MAAllocation_SetName(this, name) (this->pVtbl)->SetName (this, name)
+#define CD3D12MAAllocation_GetName(this) (this->pVtbl)->GetName (this)
+#define CD3D12MAAllocation_WasZeroInitialized(this) (this->pVtbl)->WasZeroInitialized (this)
+
 typedef struct CD3D12MAAllocatorVtbl
 {
   void (*Release) (CD3D12MAAllocator *This);
+
   void (*GetD3D12Options) (CD3D12MAAllocator *This,
                            const D3D12_FEATURE_DATA_D3D12_OPTIONS **ppOptions);
+  bool (*IsUMA) (CD3D12MAAllocator *This);
+
+  bool (*IsCacheCoherentUMA) (CD3D12MAAllocator *This);
+
+  HRESULT (*CreateResource)
+  (CD3D12MAAllocator *This,
+   const CD3D12MA_ALLOCATION_DESC *pAllocDesc,
+   const D3D12_RESOURCE_DESC *pResourceDesc,
+   D3D12_RESOURCE_STATES InitialResourceState,
+   const D3D12_CLEAR_VALUE *pOptimizedClearValue,
+   CD3D12MAAllocation **ppAllocation,
+   REFIID iidResource,
+   void **ppvResource);
+
+#ifdef __ID3D12Device4_INTERFACE_DEFINED__
+  HRESULT (*CreateResource1)
+  (CD3D12MAAllocator *This,
+   const CD3D12MA_ALLOCATION_DESC *pAllocDesc,
+   const D3D12_RESOURCE_DESC *pResourceDesc,
+   D3D12_RESOURCE_STATES InitialResourceState,
+   const D3D12_CLEAR_VALUE *pOptimizedClearValue,
+   ID3D12ProtectedResourceSession *pProtectedSession,
+   CD3D12MAAllocation **ppAllocation,
+   REFIID iidResource,
+   void **ppvResource);
+#endif
+
+#ifdef __ID3D12Device8_INTERFACE_DEFINED__
+  HRESULT (*CreateResource2)
+  (CD3D12MAAllocator *This,
+   const CD3D12MA_ALLOCATION_DESC *pAllocDesc,
+   const D3D12_RESOURCE_DESC1 *pResourceDesc,
+   D3D12_RESOURCE_STATES InitialResourceState,
+   const D3D12_CLEAR_VALUE *pOptimizedClearValue,
+   ID3D12ProtectedResourceSession *pProtectedSession,
+   CD3D12MAAllocation **ppAllocation,
+   REFIID iidResource,
+   void **ppvResource);
+#endif
+
+  HRESULT (*AllocateMemory)
+  (CD3D12MAAllocator *This,
+   const CD3D12MA_ALLOCATION_DESC *pAllocDesc,
+   const D3D12_RESOURCE_ALLOCATION_INFO *pAllocInfo,
+   CD3D12MAAllocation **ppAllocation);
+
+#ifdef __ID3D12Device4_INTERFACE_DEFINED__
+  HRESULT (*AllocateMemory1)
+  (CD3D12MAAllocator *This,
+   const CD3D12MA_ALLOCATION_DESC *pAllocDesc,
+   const D3D12_RESOURCE_ALLOCATION_INFO *pAllocInfo,
+   ID3D12ProtectedResourceSession *pProtectedSession,
+   CD3D12MAAllocation **ppAllocation);
+#endif
+
+  HRESULT (*CreateAliasingResource)
+  (CD3D12MAAllocator *This,
+   CD3D12MAAllocation *pAllocation,
+   UINT64 AllocationLocalOffset,
+   const D3D12_RESOURCE_DESC *pResourceDesc,
+   D3D12_RESOURCE_STATES InitialResourceState,
+   const D3D12_CLEAR_VALUE *pOptimizedClearValue,
+   REFIID iidResource,
+   void **ppvResource);
+
+  HRESULT (*CreatePool)
+  (CD3D12MAAllocator *This, const CD3D12MA_POOL_DESC *pPoolDesc, CD3D12MAPool **ppPool);
+
+  void (*SetCurrentFrameIndex) (CD3D12MAAllocator *This, UINT frameIndex);
+
+  void (*CalculateStats) (CD3D12MAAllocator *This, CD3D12MA_STATS *pStats);
+
+  void (*GetBudget) (CD3D12MAAllocator *This,
+                     CD3D12MA_BUDGET *pGpuBudget,
+                     CD3D12MA_BUDGET *pCpuBudget);
 } CD3D12MAAllocatorVtbl;
 
 struct CD3D12MAAllocator
@@ -117,7 +250,57 @@ struct CD3D12MAAllocator
 
 #define CD3D12MAAllocator_Release(this) (this->pVtbl)->Release (this)
 #define CD3D12MAAllocator_GetD3D12Options(this, ppOptions)                                        \
-  (this->pVtbl)->GetD3D12Options (this, &ppOptions)
+  (this->pVtbl)->GetD3D12Options (this, &(ppOptions))
+#define CD3D12MAAllocator_IsUMA(this) (this->pVtbl)->IsUMA (this)
+#define CD3D12MAAllocator_IsCacheCoherentUMA(this) (this->pVtbl)->IsCacheCoherentUMA (this)
+#define CD3D12MAAllocator_CreateResource(this, pAllocDesc, pResourceDesc, InitialResourceState,   \
+                                         pOptimizedClearValue, ppAllocation, iidResource,         \
+                                         ppvResource)                                             \
+  (this->pVtbl)                                                                                   \
+      ->CreateResource (this, pAllocDesc, pResourceDesc, InitialResourceState,                    \
+                        pOptimizedClearValue, ppAllocation, iidResource, ppvResource)
+
+#ifdef __ID3D12Device4_INTERFACE_DEFINED__
+#define CD3D12MAAllocator_CreateResource1(this, pAllocDesc, pResourceDesc, InitialResourceState,  \
+                                          pOptimizedClearValue, pProtectedSession, ppAllocation,  \
+                                          iidResource, ppvResource)                               \
+  (this->pVtbl)                                                                                   \
+      ->CreateResource (this, pAllocDesc, pResourceDesc, InitialResourceState,                    \
+                        pOptimizedClearValue, pProtectedSession, ppAllocation, iidResource,       \
+                        ppvResource)
+#endif
+
+#ifdef __ID3D12Device8_INTERFACE_DEFINED__
+#define CD3D12MAAllocator_CreateResource2(this, pAllocDesc, pResourceDesc, InitialResourceState,  \
+                                          pOptimizedClearValue, pProtectedSession, ppAllocation,  \
+                                          iidResource, ppvResource)                               \
+  (this->pVtbl)                                                                                   \
+      ->CreateResource (this, pAllocDesc, pResourceDesc, InitialResourceState,                    \
+                        pOptimizedClearValue, pProtectedSession, ppAllocation, iidResource,       \
+                        ppvResource)
+#endif
+#define CD3D12MAAllocator_AllocateMemory(this, pAllocDesc, pAllocInfo, ppAllocation)              \
+  (this->pVtbl)->AllocateMemory (this, pAllocDesc, pAllocInfo, ppAllocation)
+#ifdef __ID3D12Device4_INTERFACE_DEFINED__
+#define CD3D12MAAllocator_AllocateMemory1(this, pAllocDesc, pAllocInfo, pProtectedSession,        \
+                                          ppAllocation)                                           \
+  (this->pVtbl)->AllocateMemory1 (this, pAllocDesc, pAllocInfo, pProtectedSession, ppAllocation)
+#endif
+#define CD3D12MAAllocator_CreateAliasingResource(this, pAllocation, AllocationLocalOffset,        \
+                                                 pResourceDesc, InitialResourceState,             \
+                                                 pOptimizedClearValue, iidResource, ppvResource)  \
+  (this->pVtbl)                                                                                   \
+      ->CreateAliasingResource (this, pAllocation, AllocationLocalOffset, pResourceDesc,          \
+                                InitialResourceState, pOptimizedClearValue, iidResource,          \
+                                ppvResource)
+#define CD3D12MAAllocator_CreatePool(this, pPoolDesc, ppPool)                                     \
+  (this->pVtbl)->CreatePool (this, pPoolDesc, ppPool)
+#define CD3D12MAAllocator_SetCurrentFrameIndex(this, index)                                       \
+  (this->pVtbl)->SetCurrentFrameIndex (this, index)
+#define CD3D12MAAllocator_CalculateStats(this, pStats) (this->pVtbl)->CalculateStats (this, pStats)
+#define CD3D12MAAllocator_GetBudget(this, pGpuBudget, pCpuBudget)                                 \
+  (this->pVtbl)->GetBudget (this, pGpuBudget, pCpuBudget)
+
 
 HRESULT
 CreateAllocator (const CD3D12MA_ALLOCATOR_DESC *pDesc, CD3D12MAAllocator **ppAllocator);
